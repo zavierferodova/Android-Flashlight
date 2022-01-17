63b43b2ddf6acdf1e16fd63114e8da46256e02a6
zavierferodova <zavierferodova@gmail.com>
2022-01-17 16:55:12 +0700

    Update app icon

--- app/src/main/java/com/zavierdev/flashlight/MainActivity.kt
+++ app/src/main/java/com/zavierdev/flashlight/MainActivity.kt
@@ -5,7 +5,6 @@ import android.hardware.camera2.CameraManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import androidx.annotation.RequiresApi
 import com.zavierdev.flashlight.databinding.ActivityMainBinding
